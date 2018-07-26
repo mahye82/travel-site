0b9857ea5e28c197c338afd6fb6328f467fc57d8
MA Hye <mahye.82@gmail.com>
2018-07-26 18:13:30 +0100

    Modularise CSS using postcss-import plugin

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ const gulp = require('gulp'),
     postcss = require('gulp-postcss'),
     autoprefixer = require('autoprefixer'),
     cssvars = require('postcss-simple-vars'),
-    nested = require('postcss-nested');
+    nested = require('postcss-nested'),
+    cssImport = require('postcss-import');
 
 // https://github.com/gulpjs/gulp/blob/v3.9.1/docs/API.md#gulptaskname--deps-fn
 // Define the default task. Invoking 'gulp' from the terminal will run this task.
@@ -20,11 +21,11 @@ gulp.task('html', function () {
 // task.
 //
 // It takes './app/assets/styles/styles.css' as the source file for a stream that is piped to the post-CSS preprocessor
-// which applies the postcss-nested, postcss-simple-vars and autoprefixer packages. Then this is piped again to a
-// destination folder'./app/temp/styles'.
+// which applies a variety of PostCSS plugins. Then this is piped again to a destination folder'./app/temp/styles'.
 //
 // In short, we're taking our styles.css which contains invalid CSS like variables, nesting (as well as valid but
-// unvendorized rules). We convert that into valid CSS and spewing out the valid CSS file into another folder.
+// unvendorized rules, importing CSS files). We convert that into valid CSS and spewing out the valid CSS file into
+// another folder. That new CSS file is what's used in index.html.
 //
 // https://gulpjs.org/api#gulpsrcglobs-options
 // https://nodejs.org/api/stream.html#stream_readable_pipe_destination_options
@@ -33,7 +34,7 @@ gulp.task('html', function () {
 gulp.task('styles', function () {
     // Requires a return statement because .src is async, and we want gulp to be aware when these operations complete.
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssvars, nested, autoprefixer]))
+        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
         .pipe(gulp.dest('./app/temp/styles'));
 });
 
