fd754bec9bc5eefcead4712fdc940b4f6c300cb2
MA Hye <mahye.82@gmail.com>
2018-08-13 18:10:40 +0100

    Remove files used for testing webpack functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,19 +1,1 @@
-// Making use of Node's require function to import a module
-// const Person = require('./modules/Person');
 
-// Use ES6 to import a module
-import Person from './modules/Person';
-
-class Adult extends Person {
-    payTaxes() {
-        console.log(this.name + " now owes $0 in taxes");
-    }
-}
-
-alert("ABC 321");
-
-const john = new Person("John Doe", "blue");
-john.greet();
-const jane = new Adult("Jane Doe", "orange");
-jane.greet();
-jane.payTaxes();
