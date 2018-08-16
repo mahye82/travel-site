2b1387aa8571166be1a34f974fc55b1e76569196
MA Hye <mahye.82@gmail.com>
2018-08-16 16:53:51 +0100

    Add highlight to nav link for current page section upon scroll

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -11313,13 +11313,19 @@ var StickyHeader = function () {
         // Select DOM elements and attach them as properties to this StickyHeader instance
         this.siteHeader = (0, _jquery2.default)(".site-header");
         this.headerTriggerElement = (0, _jquery2.default)(".large-hero__title");
+        this.pageSections = (0, _jquery2.default)(".page-section");
+        this.headerLinks = (0, _jquery2.default)(".primary-nav a");
 
         // Create the Waypoint for the Header when the page loads
         this.createHeaderWaypoint();
+
+        // Create Waypoints for each page section when the page loads
+        this.createPageSectionWaypoints();
     }
 
     /**
-     * Creates a Waypoint for every element we've selected to be revealed when they are scrolled to.
+     * Creates a Waypoint for a position on the page (the large-hero__title element). When that position is scrolled to
+     * by the user, the header background is made darker and the header logo is minimised.
      * // http://imakewebthings.com/waypoints/guides/getting-started/
      *
      * A Waypoint defines:
@@ -11352,6 +11358,83 @@ var StickyHeader = function () {
                         that.siteHeader.removeClass("site-header--dark");
                     }
                 }
+            });
+        }
+
+        /**
+         * Creates Waypoints for each page-section element. When the user scrolls to one of these Waypoints, the
+         * text for the corresponding link in the nav bar turns orange.
+         *
+         * (This is achieved by making use of custom data attributes. Each header link gets its own id property. Then each
+         * page-section gets a custom data attribute called data-matching-link. Then when a Waypoint is reached, we just
+         * extract the data-matching-link's value. This value will be the ID for the corresponding header link. We use this
+         * ID to give it a class that turns it orange.)
+         *
+         * https://www.w3schools.com/tags/att_global_data.asp
+         *
+         * A Waypoint defines:
+         * 1) element - The DOM element we want to watch for as we scroll down the page.
+         * 2) handler - The function we want to fire when the DOM element we are watching is scrolled to.
+         * 3) offset - An optional value. By default, waypoints only triggers the handler when the user has scrolled so
+         *              that the element has reached the top of the viewport (0%). We can offset it so that the handler
+         *              sooner. For example, if we want the handler to fire when the element is near the bottom of the
+         *              viewport we could set the offset to 100%.
+         *
+         * http://imakewebthings.com/waypoints/guides/getting-started/
+         */
+
+    }, {
+        key: 'createPageSectionWaypoints',
+        value: function createPageSectionWaypoints() {
+            // Because the value of 'this' will change when we're using the new keyword for the Waypoint below, we declare
+            // 'that' to refer to an instance of the StickyHeader class. This will allow us to access the propertoes on the
+            // StickyHeader instance when we're creating a new Waypoint.
+            var that = this;
+
+            // each() is a JQuery method: http://api.jquery.com/jquery.each/
+            this.pageSections.each(function () {
+                // jQuery's each function sets the value of the 'this' keyword to be the current element in the selection
+                // that is being looped through.
+                var currentPageSection = this;
+
+                // Waypoint for scrolling down to this page-section
+                new Waypoint({
+                    element: currentPageSection,
+                    handler: function handler(direction) {
+                        if (direction === "down") {
+                            // Use the getAttribute method to get the custom data attribute. This attribute holds a
+                            // reference to the ID of a corresponding button (actually a link inside an li) in the header's
+                            // nav bar.
+                            var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+                            // Remove the orange highlight on every link in the sticky header
+                            that.headerLinks.removeClass("is-current-link");
+                            // Add the orange highlight to the correct link in the header, i.e. the link that matches the
+                            // Waypoint the user just scrolled to
+                            (0, _jquery2.default)(matchingHeaderLink).addClass("is-current-link");
+                        }
+                    },
+                    offset: "18%" // Trigger the handler when the element is 18% from the top of the viewport
+                });
+
+                // Waypoint for scrolling up to this page-section
+                new Waypoint({
+                    element: currentPageSection,
+                    handler: function handler(direction) {
+                        if (direction === "up") {
+                            // Use the getAttribute method to get the custom data attribute. This attribute holds a
+                            // reference to the ID of a corresponding button (actually a link inside an li) in the header's
+                            // nav bar.
+                            var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+                            // Remove the orange highlight on every link in the sticky header
+                            that.headerLinks.removeClass("is-current-link");
+                            // Add the orange highlight to the correct link in the header, i.e. the link that matches the
+                            // Waypoint the user just scrolled to
+                            (0, _jquery2.default)(matchingHeaderLink).addClass("is-current-link");
+                        }
+                    },
+                    offset: "-40%" // Trigger the handler when the start of the element is 40% from the top of
+                    // the viewport. Negative number required for offsetting when scrolling up.
+                });
             });
         }
     }]);
