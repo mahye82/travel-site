e64fae55c9f277f35b70016f23b066146020414c
MA Hye <mahye.82@gmail.com>
2018-08-22 18:46:31 +0100

    Create gulp task for deleting dist folder and its contents

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,5 +1,12 @@
 const gulp = require('gulp'),
-    imagemin = require('gulp-imagemin');            // https://github.com/sindresorhus/gulp-imagemin
+    imagemin = require('gulp-imagemin'),            // https://github.com/sindresorhus/gulp-imagemin
+    del = require('del');                           // https://www.npmjs.com/package/del
+
+// This gulp task is a dependency of the 'gulp build' task. It is responsible for deleting the dist folder and its
+// contents. We do this so that we start from a blank state before any of the other build tasks run.
+gulp.task('deleteDistFolder', function () {
+    return del('./dist');
+});
 
 // This gulp task is a dependency of the 'gulp build' task. It is responsible for compressing the image files in the
 // app folder and copying them over to the dist folder.
@@ -7,7 +14,9 @@ const gulp = require('gulp'),
 // Gulp to take as source as everything in the ./app/assets/images/ folder except the /icons folder, and pipes it as
 // a stream to the gulp-imagemin module. imagemin optimises the images using the given plugin options. Then the result
 // is streamed to the destination directory ./dist/assets/images.
-gulp.task('optimizeImages', function () {
+//
+// The second argument is the list of dependencies for this task, i.e. the tasks we want to run before this one.
+gulp.task('optimizeImages', ['deleteDistFolder'], function () {
     // Requires a return statement because .src is async, and we want gulp to be aware when these operations complete.
     // '!' indicates files that we want to exclude from those added by * wildcards.
    return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons', '!././app/assets/images/icons/**/*'])
@@ -25,4 +34,4 @@ gulp.task('optimizeImages', function () {
 //
 // Invoking 'gulp build' will create/update the dist directory - just the compressed production files needed for our
 // server (not our organised, uncompressed dev/source files).
-gulp.task('build', ['optimizeImages']);
+gulp.task('build', ['deleteDistFolder', 'optimizeImages']);
